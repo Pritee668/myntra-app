eeea5966530e3cf4c79462f234c7e7b3ee83518d
Pritee Gupta <136907754+Pritee668@users.noreply.github.com>
2024-07-12 17:35:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import StyleFeed from './components/StyleFeed';
-import SocialLookbook from './components/SocialLookBook';
-import SocialChallenges from './components/SocialChallenges';
 function App() {
   return (
     <div className="App">
-      <SocialChallenges/>
-     {/* <SocialLookbook/>
-      <StyleFeed/> */}
+    
+<h1>APP component</h>
     </div>
   );
 }
